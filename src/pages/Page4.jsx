58a20782421fe31d5669a459f45e174be30bd6d0
Anandhukb0007 <anandhukb0007@gmail.com>
2025-01-29 22:16:17 +0530

    Update dependencies and enhance Page4 layout with new styles and project details

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -1,5 +1,7 @@
 import "./Page4.css";
 import spotify from "../images/Spotify.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 function Page4() {
   return (
@@ -13,19 +15,32 @@ function Page4() {
             <div className="project-title">
               <h1>Spotify UI</h1>
             </div>
-            <div className="project-image">
-              <img src={spotify} alt="Spotify UI" />
-            </div>
-            <div className="project-description">
-              <p>
-                My Spotify UI front-end project is a responsive web application
-                built using React, designed to replicate Spotify's sleek and
-                intuitive interface. It features a home page for playlists and
-                recommendations, a search page to find songs and artists, and a
-                library section for managing user playlists. The app includes a
-                persistent music player with playback controls and incorporates
-                smooth animations to enhance the user experience.
-              </p>
+            <div className="project-details">
+              <div className="p-lcolumn">
+                <div className="project-image">
+                  <img src={spotify} alt="Spotify UI" />
+                </div>
+              </div>
+              <div className="p-rcolumn">
+                <div className="project-description">
+                  <p>
+                    My Spotify UI front-end project is a responsive web
+                    application built using React, designed to replicate
+                    Spotify's sleek and intuitive interface. It features a home
+                    page for playlists and recommendations, a search page to
+                    find songs and artists, and a library section for managing
+                    user playlists. The app includes a persistent music player
+                    with playback controls and incorporates smooth animations to
+                    enhance the user experience.
+                  </p>
+                </div>
+                <div className="project-btn">
+                  <button className="p-btn">VIEW CODE</button>
+                  <button className="p-btn">
+                    <FontAwesomeIcon icon={faPlay} className="play" /> LIVE
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
