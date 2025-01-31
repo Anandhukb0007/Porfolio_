778e24a79bd2da044fd969a3326e2baab968b0f7
Anandhukb0007 <anandhukb0007@gmail.com>
2025-01-31 12:43:26 +0530

    Refactor Page4 layout to use row flex-direction and add project details for Spotify UI

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -37,6 +37,32 @@ function Page4() {
               </div>
             </div>
           </div>
+          <div className="project1">
+            <div className="project-title">
+              <h1>Spotify UI</h1>
+            </div>
+            <div className="project-details">
+              <div className="p-lcolumn">
+                <div className="project-image">
+                  <img src={spotify} alt="Spotify UI" />
+                </div>
+              </div>
+              <div className="p-rcolumn">
+                <div className="project-description">
+                  <p>
+                    My Spotify UI front-end project is a responsive web
+                    application built using React, designed to replicate
+                    Spotify's sleek and intuitive interface.</p>
+                </div>
+                <div className="project-btn">
+                  <button className="p-btn">VIEW CODE</button>
+                  <button className="p-btn">
+                    <FontAwesomeIcon icon={faPlay} className="play" /> LIVE
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
