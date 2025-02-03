a2a92239747c90e0236efe8276c408b6bf091a89
Anandhukb0007 <anandhukb0007@gmail.com>
2025-02-03 19:31:19 +0530

    Add technologies section to Page3 and enhance Page4 styles with new margins and borders

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -54,6 +54,143 @@ function Page3() {
           </div>
         </div>
       </div>
+
+      <div className="technologies">
+        <div className="technologies-header">
+          <h1>TECHNOLOGIES</h1>
+        </div>
+
+        <div className="technologies-content">
+          <div className="icon-container">
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/html-5--v1.png"
+                  alt="html-5--v1"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">HTML</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/css3.png"
+                  alt="css3"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">CSS</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/javascript--v1.png"
+                  alt="javascript--v1"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">JAVASCRIPT</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/react-native.png"
+                  alt="react-native"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">REACT</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/nodejs.png"
+                  alt="nodejs"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">NODEJS</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/mongodb.png"
+                  alt="mongodb"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">MONGODB</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/git.png"
+                  alt="git"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">GIT</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/github--v1.png"
+                  alt="github--v1"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">GITHUB</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+                <img
+                  width="144"
+                  height="144"
+                  src="https://img.icons8.com/color/144/bootstrap.png"
+                  alt="bootstrap"
+                />
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">BOOTSTRAP</p>
+              </div>
+            </div>
+            <div className="icon">
+              <div className="icon-img">
+              <img width="100" height="100" src="https://img.icons8.com/ios/100/FFFFFF/express-js.png" alt="express-js"/>
+              </div>
+              <div className="icon-text">
+                <p className="icon-p">EXPRESSJS</p>
+              </div>
+              </div>
+              
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
