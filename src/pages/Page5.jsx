7bd66f190133c6de765ec2e0af62ee2a07e08913
Anandhukb0007 <anandhukb0007@gmail.com>
2025-02-06 22:02:50 +0530

    Add contact section to Page5 with icons for email, Instagram, and LinkedIn

--- src/pages/Page5.jsx
+++ src/pages/Page5.jsx
@@ -1,24 +1,36 @@
 import "./Page5.css";
+import email from "../images/email.png";
+import insta from "../images/instagram.png";
+import linkedin from "../images/linkedin.png";
 
 function Page5() {
-    return ( 
-        <div className="page-5">
-            <div className="page-header">
-                <div className="header">CONTACTS</div>
-            </div>
-            <div className="contact-container">
-                <div className="contact">
-                    <div className="contact-title">
-                    <a href="https://www.flaticon.com/free-icons/email" title="email icons">Email icons created by Smashicons - Flaticon</a>
-                        <h1>EMAIL</h1>
-                    </div>
-                    </div>
-                
-                </div>
-
-
+  return (
+    <div className="page-5">
+      <div className="page-header">
+        <div className="header">CONTACTS</div>
+      </div>
+      <div className="contact-container">
+        <div className="contact">
+          <div className="contact-icon">
+            <img src={email} alt="email" />
+            
+          </div>
         </div>
-     );
+        <div className="contact">
+            <div className="contact-icon">
+                <img src={insta} alt="instagram" />
+                
+            </div>
+        </div>
+        <div className="contact">
+            <div className="contact-icon">
+                <img src={linkedin} alt="linkedin" />
+                
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page5;
